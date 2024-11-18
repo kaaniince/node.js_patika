5d3f655519d33da0dcda6226b5edeb8566ed43bc
Kaan Ince <kaaniince@outlook.com>
2024-11-18 15:18:10 +0300

    object destructured

---
+++ moduleCircle/index.js
@@ -0,0 +1,5 @@
+const { circleArea, circleCircumference } = require("./circle"); //object destructing
+
+const radius = 5;
+console.log("Daire Alanı:", circleArea(radius));
+console.log("Daire Çevresi:", circleCircumference(radius));
