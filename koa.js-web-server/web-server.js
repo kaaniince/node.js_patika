ed2bf5d4cfa1ae6b166820d52a9f76999bbf58d7
Kaan Ince <kaaniince@outlook.com>
2024-11-19 18:19:04 +0300

    404 error added

--- koa.js-web-server/web-server.js
+++ koa.js-web-server/web-server.js
@@ -28,6 +28,12 @@ app.use(async (ctx, next) => {
   }
 });
 
+// 404 Not Found page
+app.use(async (ctx) => {
+  ctx.status = 404;
+  ctx.body = "<h1>404 Sayfa Bulunamadı</h1>";
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
